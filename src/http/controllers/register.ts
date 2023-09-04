93ba9eebebf9c8b87442bd7f7db562b2501d672f
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-09-04 20:57:19 -0300

    feat: password hash and validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,23 +1,38 @@
 import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
 export async function register (request: FastifyRequest, reply: FastifyReply)  {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
-    });
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  });
 
-    const { email, name, password } = registerBodySchema.parse(request.body);
+  // $2a$06$S3xD4knSI24XirDP7nt6h.2wntsommCjEdtwJZmq8v0pgGCrVREAW
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password
-        }
-    });
+  const { email, name, password } = registerBodySchema.parse(request.body);
 
-    return reply.status(201).send();
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+
+  if(userWithSameEmail) {
+    return reply.status(409).send();
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password_hash
+    }
+  });
+
+  return reply.status(201).send();
 }
