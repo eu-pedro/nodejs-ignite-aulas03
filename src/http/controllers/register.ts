655c0a4acaae042c401efde5f4906f26898cd237
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-09-04 21:18:09 -0300

    feat: use case register

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,38 +1,23 @@
-import { prisma } from '@/lib/prisma';
-import { hash } from 'bcryptjs';
+import { registerUseCase } from '@/use-cases/register';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
-export async function register (request: FastifyRequest, reply: FastifyReply)  {
-  const registerBodySchema = z.object({
+export async function register (request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object( {
     name: z.string(),
     email: z.string().email(),
-    password: z.string().min(6)
-  });
+    password: z.string().min( 6 )
+  } );
 
   // $2a$06$S3xD4knSI24XirDP7nt6h.2wntsommCjEdtwJZmq8v0pgGCrVREAW
 
-  const { email, name, password } = registerBodySchema.parse(request.body);
+  const { email, name, password } = registerBodySchema.parse( request.body );
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email
-    }
-  });
-
-  if(userWithSameEmail) {
+  try {
+    await registerUseCase({name, email, password});
+  } catch (error) {
     return reply.status(409).send();
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password_hash
-    }
-  });
-
-  return reply.status(201).send();
+  return reply.status( 201 ).send();
 }
