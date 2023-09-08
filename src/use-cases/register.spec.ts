5b38c6b479491a7003fc022cc1b4498700c0e414
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-09-08 10:43:33 -0300

    feat: first unit test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,34 @@
-import { expect, test } from 'vitest';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { describe, expect, it } from 'vitest';
+import { RegisterUseCase } from './register';
+import { compare } from 'bcryptjs';
 
-test('check if it works', () => {
-  expect(2+2).toEqual(4);
+describe('Register Use Case', () => {
+  it('should hash user password upon registrations', async () => {
+    
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail (email) {
+        return null;
+      },
+      async create (data) {
+        return { 
+          id: 'user-id',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      }
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'john.doe@email.com',
+      password: '123456'
+    });
+
+    const isPasswordCorrectlyHashed = await compare('123456', user.password_hash);
+
+    expect(isPasswordCorrectlyHashed).toBeTruthy();
+  });
 });
