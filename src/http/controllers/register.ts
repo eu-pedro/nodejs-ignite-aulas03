6aef36ad5a9a3f3050c92f657893df26d4f3ec34
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-09-10 10:01:05 -0300

    feat: add factory pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
-import { RegisterUseCase } from '@/use-cases/register';
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
@@ -16,9 +15,7 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
   const { email, name, password } = registerBodySchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const registerUseCase = makeRegisterUseCase();
 
     await registerUseCase.execute({name,
       email,
@@ -31,5 +28,5 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
     throw error;
   }
 
-  return reply.status( 201 ).send();
+  return reply.status(201).send();
 }
