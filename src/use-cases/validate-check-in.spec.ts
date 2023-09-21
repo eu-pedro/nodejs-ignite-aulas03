20434569b45076041f0e5a3571dd4e5912d135a7
pedro henrique <pedro.henrike2812@gmail.com>
2023-09-21 10:38:27 -0300

    feat: validating check-in time

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -3,6 +3,7 @@ import { beforeEach } from 'vitest';
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 import { ValidateCheckInUseCase } from './validate-check-in';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error';
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -56,6 +57,6 @@ describe('Validate Check-in  Use Case', () => {
     
     await expect(() => sut.execute({
       checkInId: createdCheckIn.id
-    })).rejects.toBeInstanceOf(Error);
+    })).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
