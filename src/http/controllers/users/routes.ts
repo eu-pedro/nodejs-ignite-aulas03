09c7e79429aa0d41bbcaf4d6555bc7a7312472c9
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-11-15 17:26:44 -0300

    feat: create other gym controllers

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from 'fastify';
+
+import { register } from './register';
+import { authenticate } from './authenticate';
+import { profile } from './profile';
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt';
+
+export async function usersRoutes (app:FastifyInstance) {
+  app.post('/users', register);
+
+  app.post('/sessions', authenticate);
+
+  // autenticado
+
+  app.get('/me', {
+    onRequest: [verifyJWT]
+  } ,profile);
+}
