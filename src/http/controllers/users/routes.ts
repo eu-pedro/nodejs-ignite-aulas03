f97702141a03319564fc8df511ac29171db3b3a9
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-11-25 17:20:08 -0300

    feat: implementing refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,11 +5,14 @@ import { authenticate } from './authenticate';
 import { profile } from './profile';
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
+import { refresh } from './refresh';
 
 export async function usersRoutes (app:FastifyInstance) {
   app.post('/users', register);
 
   app.post('/sessions', authenticate);
+  
+  app.patch('/token/refresh', refresh);
 
   // autenticado
 
